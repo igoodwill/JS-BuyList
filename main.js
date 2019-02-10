ccdc6cf72d61db3fc1e4c6ab75c2f2f671a4f5ca
igoodwill <igor.goodwill1@gmail.com>
2019-02-10 21:40:50 +0200

    [practice-3] JS: Add item

---
+++ main.js
@@ -0,0 +1,38 @@
+$(function () {
+    const ITEM = $("#item");
+    const ITEM_TEMPLATE = ITEM.html();
+    const ITEMS = $("#items");
+    const ITEM_NAME = $("#item-name");
+
+    ITEM.remove();
+
+    function addItem(name) {
+        var item = $(ITEM_TEMPLATE);
+        item.show();
+        item.find("#title").text(name);
+        ITEMS.append(item);
+    }
+
+    addItem("Помідори");
+    addItem("Печиво");
+    addItem("Сир");
+
+    ITEM_NAME.keyup(function(event) {
+        if (event.keyCode == 13) {
+            onAdd();
+        }
+    });
+
+    $("#add-item").click(onAdd);
+
+    function onAdd() {
+        var name = ITEM_NAME.val();
+
+        if (name != "") {
+            addItem(name);
+            ITEM_NAME.val("");
+        }
+
+        ITEM_NAME.focus();
+    }
+});
